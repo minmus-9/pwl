723fc03131132b523971114b112e95143e989664
waggly dawg <dysphemism@users.noreply.github.com>
2025-01-30 14:52:41 -0700

    * whap old boot.lisp

--- qq.lisp
+++ qq.lisp
@@ -1,10 +1,23 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; playing with quasiquote impl
-
-(print "start lb")
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; dingus to build a list by appending in linear time. it's an ad-hoc queue
+;;
+;; pwl - python with lisp, a collection of lisp evaluators for Python
+;;       https://github.com/minmus-9/pwl
+;; Copyright (C) 2025  Mark Hays (github:minmus-9)
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 (special begin$2 (lambda (__special_begin$2_a__ __special_begin$2_b__)
      (if  ;; can't remember where i saw this but i like it
